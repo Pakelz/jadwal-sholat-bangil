ea932299e46755c362fcfa04901ae2b7924a9265
Pakel <wpachmad@gmail.com>
2025-06-22 22:57:04 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const tanggal = document.getElementById("tanggal");
+const subuh = document.getElementById("subuh");
+const dzuhur = document.getElementById("dzuhur");
+const ashar = document.getElementById("ashar");
+const maghrib = document.getElementById("magrib");
+const isya = document.getElementById("isya");
+
+const getTime = () => {
+    const date = new Date();
+    const result = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    return result;
+}
+
+const clock = () => {
+    const date = new Date();
+    const second = date.getSeconds().toString().length == 2
+    ? `${date.getSeconds()}` : `0${date.getSeconds()}`;
+    const result = `${date.getHours()}:${date.getMinutes()}:${second}`;
+    return result;
+}
+
+async function getJadwalSholat() {
+    try {
+        const res = await fetch(`https://api.myquran.com/v2/sholat/jadwal/1620/${getTime()}`);
+        if (!res.ok) throw new Error("Invalid response");
+        const data = await res.json();
+        return data.data.jadwal;
+    } catch(err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+async function show_jadwal() {
+    try {
+        const data = await getJadwalSholat()
+
+        tanggal.textContent = data.tanggal;
+        subuh.textContent = data.subuh;
+        dzuhur.textContent = data.dzuhur;
+        ashar.textContent = data.ashar;
+        maghrib.textContent = data.maghrib;
+        isya.textContent = data.isya;
+
+        console.log("Jadwal Sholat: ", data);
+    } catch(err) {
+        console.error(err);
+        tanggal.textContent = "Gagal memuat jadwal"
+    }
+}
+
+setInterval(() => {
+    document.getElementById("clock").textContent = clock();
+}, 1000);
+
+show_jadwal()
